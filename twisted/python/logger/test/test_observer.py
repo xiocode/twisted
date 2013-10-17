1fdb114d55ec57dc0e1eb52b483354db902f0185
wsanchez <wsanchez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-10-17 19:44:47 +0000

    Move formatTrace test unto (new) test_util module.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/log-booyah-6750-3@40373 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/logger/test/test_observer.py
+++ twisted/python/logger/test/test_observer.py
@@ -12,7 +12,6 @@ from twisted.trial import unittest
 from twisted.python.logger._logger import Logger
 from twisted.python.logger._observer import ILogObserver
 from twisted.python.logger._observer import LogPublisher
-from twisted.python.logger._util import formatTrace
 
 
 
@@ -154,7 +153,6 @@ class LogPublisherTests(unittest.TestCase):
         pukes when the failure is reported.  Exception should still not
         propagate back to the caller.
         """
-
         event = dict(foo=1, bar=2)
         exception = RuntimeError("ARGH! EVIL DEATH!")
 
@@ -174,90 +172,19 @@ class LogPublisherTests(unittest.TestCase):
 
     def test_trace(self):
         """
-        Tracing keeps track of forwarding done by the publisher.
+        Tracing keeps track of forwarding to observers.
         """
-        publisher = LogPublisher()
+        event = dict(foo=1, bar=2, log_trace=[])
 
-        event = dict(log_trace=[])
+        traces = {}
 
-        o1 = lambda e: None
+        # Copy trace to a tuple; otherwise, both observers will store the same
+        # mutable list, and we won't be able to see o1's view distinctly.
+        o1 = lambda e: traces.setdefault(1, tuple(e["log_trace"]))
+        o2 = lambda e: traces.setdefault(2, tuple(e["log_trace"]))
 
-        def o2(e):
-            self.assertIdentical(e, event)
-            self.assertEquals(
-                e["log_trace"],
-                [
-                    (publisher, o1),
-                    (publisher, o2),
-                    # Event hasn't been sent to o3 yet
-                ]
-            )
-
-        def o3(e):
-            self.assertIdentical(e, event)
-            self.assertEquals(
-                e["log_trace"],
-                [
-                    (publisher, o1),
-                    (publisher, o2),
-                    (publisher, o3),
-                ]
-            )
-
-        publisher.addObserver(o1)
-        publisher.addObserver(o2)
-        publisher.addObserver(o3)
+        publisher = LogPublisher(o1, o2)
         publisher(event)
 
-
-    def test_formatTrace(self):
-        """
-        Format trace as string.
-        """
-        event = dict(log_trace=[])
-
-        o1 = lambda e: None
-        o2 = lambda e: None
-        o3 = lambda e: None
-        o4 = lambda e: None
-        o5 = lambda e: None
-
-        o1.name = "root/o1"
-        o2.name = "root/p1/o2"
-        o3.name = "root/p1/o3"
-        o4.name = "root/p1/p2/o4"
-        o5.name = "root/o5"
-
-        def testObserver(e):
-            self.assertIdentical(e, event)
-            trace = formatTrace(e["log_trace"])
-            self.assertEquals(
-                trace,
-                (
-                    u"{root} ({root.name})\n"
-                    u"  -> {o1} ({o1.name})\n"
-                    u"  -> {p1} ({p1.name})\n"
-                    u"    -> {o2} ({o2.name})\n"
-                    u"    -> {o3} ({o3.name})\n"
-                    u"    -> {p2} ({p2.name})\n"
-                    u"      -> {o4} ({o4.name})\n"
-                    u"  -> {o5} ({o5.name})\n"
-                    u"  -> {oTest}\n"
-                ).format(
-                    root=root,
-                    o1=o1, o2=o2, o3=o3, o4=o4, o5=o5,
-                    p1=p1, p2=p2,
-                    oTest=oTest
-                )
-            )
-        oTest = testObserver
-
-        p2 = LogPublisher(o4)
-        p1 = LogPublisher(o2, o3, p2)
-
-        p2.name = "root/p1/p2/"
-        p1.name = "root/p1/"
-
-        root = LogPublisher(o1, p1, o5, oTest)
-        root.name = "root/"
-        root(event)
+        self.assertEquals(traces[1], ((publisher, o1),))
+        self.assertEquals(traces[2], ((publisher, o1), (publisher, o2)))
