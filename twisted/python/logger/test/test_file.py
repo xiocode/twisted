1ea3973f20ca6ea26e84599b3b093d1aa91d82df
wsanchez <wsanchez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-10-23 02:15:48 +0000

    Clean up test_file now that most tests are moved to text_format.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/log-booyah-6750-3@40437 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/logger/test/test_file.py
+++ twisted/python/logger/test/test_file.py
@@ -6,23 +6,14 @@ Test cases for L{twisted.python.logger._file}.
 """
 
 from io import StringIO
-from time import mktime
-
-try:
-    from time import tzset
-    # We should upgrade to a version of pyflakes that does not require this.
-    tzset
-except ImportError:
-    tzset = None
 
 from zope.interface.verify import verifyObject, BrokenMethodImplementation
 
 from twisted.trial import unittest
-from twisted.trial.unittest import SkipTest
 
 from twisted.python.logger._observer import ILogObserver
+from twisted.python.logger._file import FileLogObserver
 from twisted.python.logger._file import textFileLogObserver
-from twisted.python.logger.test.test_format import addTZCleanup, setTZ
 
 
 
@@ -30,53 +21,6 @@ class FileLogObserverTests(unittest.TestCase):
     """
     Tests for L{FileLogObserver}.
     """
-    DEFAULT_TIMESTAMP = u"-"
-    DEFAULT_SYSTEM = u"[-#-]"
-
-    def buildOutput(self, timeStamp, system, text, encoding):
-        """
-        Build an expected output string from components.
-
-        @param timeStamp: time stamp text
-        @type timeStamp: unicode
-
-        @param system: a system name
-        @type system: unicode
-
-        @param text: a text message
-        @type text: unicode
-
-        @param encoding: an encoding
-        @type encoding: str
-
-        @return: a formatted output string that I{should} match the format
-            emitted by L{FileLogObserver}.
-        @rtype: L{unicode}
-        """
-        return (u" ".join((timeStamp, system, text)) + u"\n")
-
-
-    def buildDefaultOutput(self, text, encoding="utf-8"):
-        """
-        Build an expected output string with the default time stamp
-        and system.
-
-        @param text: a text message
-        @type text: unicode
-
-        @param encoding: an encoding
-        @type encoding: str
-
-        @return: a L{FileLogObserver}-formatted output string.
-        @rtype: bytes
-        """
-        return self.buildOutput(
-            self.DEFAULT_TIMESTAMP,
-            self.DEFAULT_SYSTEM,
-            text,
-            encoding
-        )
-
 
     def test_interface(self):
         """
@@ -84,79 +28,99 @@ class FileLogObserverTests(unittest.TestCase):
         """
         try:
             fileHandle = StringIO()
-            observer = textFileLogObserver(fileHandle)
+            observer = FileLogObserver(fileHandle, lambda e: unicode(e))
             try:
                 verifyObject(ILogObserver, observer)
             except BrokenMethodImplementation as e:
                 self.fail(e)
+
         finally:
             fileHandle.close()
 
 
-    def _testObserver(
-        self, logTime, logFormat,
-        observerKwargs, expectedOutput
-    ):
+    def test_observeWrites(self):
         """
-        Default time stamp format is RFC 3339
-
-        @param logTime: a time stamp
-        @type logTime: int
-
-        @param logFormat: a format string
-        @type logFormat: unicode
-
-        @param observerKwargs: keyword arguments for constructing the observer
-        @type observerKwargs: dict
-
-        @param expectedOutput: the expected output from the observer
-        @type expectedOutput: bytes
+        L{FileLogObserver} writes to the given file when it observes events.
         """
-        event = dict(log_time=logTime, log_format=logFormat)
+        try:
+            fileHandle = StringIO()
+            observer = FileLogObserver(fileHandle, lambda e: unicode(e))
+            event = dict(x=1)
+            observer(event)
+            self.assertEquals(fileHandle.getvalue(), unicode(event))
+
+        finally:
+            fileHandle.close()
+
+
+    def test_observeFlushes(self):
+        """
+        L{FileLogObserver} calles C{flush()} on the output file when it
+        observes an event.
+        """
+
+        class DummyFile(object):
+            def __init__(self):
+                self.flushes = 0
+
+            def write(self, data):
+                pass
+
+            def flush(self):
+                self.flushes += 1
+
+            def close(self):
+                pass
+
+        try:
+            fileHandle = DummyFile()
+            observer = FileLogObserver(fileHandle, lambda e: unicode(e))
+            event = dict(x=1)
+            observer(event)
+            self.assertEquals(fileHandle.flushes, 1)
+
+        finally:
+            fileHandle.close()
+
+
+
+class textFileLogObserverTests(unittest.TestCase):
+    """
+    Tests for L{textFileLogObserver}.
+    """
+
+    def test_returnsFileLogObserver(self):
+        """
+        L{textFileLogObserver} returns a L{FileLogObserver}.
+        """
         fileHandle = StringIO()
         try:
-            observer = textFileLogObserver(fileHandle, **observerKwargs)
-            observer(event)
-            output = fileHandle.getvalue()
-            self.assertEquals(
-                output, expectedOutput,
-                "{0!r} != {1!r}".format(expectedOutput, output)
-            )
+            observer = textFileLogObserver(fileHandle)
+            self.assertIsInstance(observer, FileLogObserver)
         finally:
             fileHandle.close()
 
 
-    def test_defaultTimeStamp(self):
+    def test_outFile(self):
         """
-        Default time stamp format is RFC 3339 and offset respects the timezone
-        as set by the standard C{TZ} environment variable and L{tzset} API.
+        Returned L{FileLogObserver} has the correct outFile.
         """
-        if tzset is None:
-            raise SkipTest(
-                "Platform cannot change timezone; unable to verify offsets."
-            )
-
-        # We only test "UTC+00" here, and rely on formatTime's tests for others
-
-        addTZCleanup(self)
-        setTZ("UTC+00")
-
-        # On some rare platforms (FreeBSD 8?  I was not able to reproduce
-        # on FreeBSD 9) 'mktime' seems to always fail once tzset() has been
-        # called more than once in a process lifetime.  I think this is
-        # just a platform bug, so let's work around it.  -glyph
+        fileHandle = StringIO()
         try:
-            localDST = mktime((2006, 6, 30, 0, 0, 0, 4, 181, 1))
-        except OverflowError:
-            raise SkipTest(
-                "Platform cannot construct time zone for 'UTC+00'"
-            )
+            observer = textFileLogObserver(fileHandle)
+            self.assertIdentical(observer._outFile, fileHandle)
+        finally:
+            fileHandle.close()
 
-        self._testObserver(
-            localDST, u"XYZZY",
-            dict(),
-            self.buildOutput(
-                u"2006-06-30T00:00:00+0000", self.DEFAULT_SYSTEM,
-                u"XYZZY", "utf-8",
-            ),
-        )
+
+    def test_timeFormat(self):
+        """
+        Returned L{FileLogObserver} has the correct outFile.
+        """
+        fileHandle = StringIO()
+        try:
+            observer = textFileLogObserver(fileHandle, timeFormat=u"%f")
+            observer(dict(log_format=u"XYZZY", log_time=1.23456))
+            self.assertEquals(fileHandle.getvalue(), u"234560 [-#-] XYZZY\n")
+        finally:
+            fileHandle.close()
