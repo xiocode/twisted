aade96e5b3487447be7461d3310f754a5a0f1646
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-11-13 07:15:55 +0000

    We added that log_source key, explain it.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/log-booyah-6750-3@40706 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ doc/core/howto/listings/logger/logsource.py
@@ -0,0 +1,11 @@
+from twisted.python.logger import Logger
+class MyObject(object):
+    log = Logger()
+    def __init__(self, value):
+        self.value = value
+    def doSomething(self, something):
+        self.log.critical(
+            "object with value {log_source.value} doing {something}",
+            something=something
+        )
+MyObject(7).doSomething("a task")
